fc435f6991a2e662f9467be60c7a495661457743
Papsan <papsanlysenko@gmail.com>
2024-03-01 21:32:11 +0200

    basic hello world app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+mod tui;
+
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    widgets::Paragraph,
+    Frame,
+};
+use std::{
+    io::{stdout, Result},
+    time::Duration,
+};
+
+fn draw(frame: &mut Frame) {
+    let area = frame.size();
+    frame.render_widget(
+        Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+            .white()
+            .on_blue(),
+        area,
+    )
+}
+
+fn main() -> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    loop {
+        terminal.draw(draw)?;
+        if event::poll(Duration::from_millis(16))? {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
